0c293813898878cf83d970fed7defdbb78d52094
poornabhaskar <poornabhaskar.duvvari@gmail.com>
2021-03-27 15:46:27 +0530

    Initial commit

---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { AppConstants } from 'src/common/app.constants';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  databaseStatus=false
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getDashboardData().subscribe((data)=>{
+      console.log("Server API Data:-- "+ JSON.stringify( data)); 
+      if(data.databaseStatus=="Y"){
+        this.databaseStatus=true
+      }
+
+    }) 
+    
+  }
+  getDashboardData(): Observable<any> {
+    const url = AppConstants.appServerURL+"/api/";
+    return this.http.get(url).pipe(map((response:any) => response));
+  }
+
+}
---
+++ src/common/app.constants.ts
@@ -0,0 +1,3 @@
+export class AppConstants {
+    public static appServerURL="http://localhost:8090";
+}
